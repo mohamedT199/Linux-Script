a1808fb96a5f009975ad6c78fc9755b214ef11ce
Administrator <admin@kali.com>
2022-12-06 17:17:20 -0500

    add feature install programs automatically to the system for manage Network Traffic

--- Staticties-Network-Adapter.sh
+++ Staticties-Network-Adapter.sh
@@ -17,10 +17,38 @@ else
 fi
 
 vnstati -vs -o /home/$user/Summries-Network-Traffic/summery.png
+
 result=$( echo "$?" )
+
+distro=$( lsb_release -i | cut -d: -f2 | sed s/'^\t'// | tr [:upper:] [:lower:] )
+
 if [ "$result" -ne 0 ]
 then 
-	echo "Please Install vnstat previuios" 
+	read -p "need to install vnstat to monitor the Network (yes / no )" request 
+
+	if [ "$request" == "yes" ]
+	then
+		if [[ "$distro" == "ubuntu"  ||  "$distro" == "kubuntu" || "$distro" == "kali"  ]]
+		then 
+			echo "Install Start "  
+			sudo apt install vnstat
+			sudo apt install vnstati
+			vnstati -vs -o /home/$user/Summries-Network-Traffic/summery.png
+			echo "New Monitor Screen Applied " 
+
+		elif [[ "$distro" == "redhat" || "$distro" == "fedora" || "$distro" == "rokey" ]]
+		then 
+			echo "Install Start " 
+			sudo yum install vnstat
+			vnstati -vs -o /home/$user/Summries-Network-Traffic/summery.png
+			echo "New Monitor Screen Applied " 
+
+		fi
+	else
+		echo "PLease instal then Manage Network Traffic"
+		
+	fi	
+	 	 	
 else 
 
 	echo "New Monitor Screen Applied " 
